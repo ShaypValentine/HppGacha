adff2292836a4e728eac1dd002710c78173c2880
ShaypValentine <shaycaith@gmail.com>
2022-07-11 22:35:32 +0200

    Hopefully http

--- main.go
+++ main.go
@@ -37,4 +37,14 @@ func main() {
 	if err := http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/hppgacha.art/fullchain.pem", "/etc/letsencrypt/live/hppgacha.art/privkey.pem", nil); err != nil {
 		log.Fatal(err)
 	}
+
+	go func() {
+		if err := http.ListenAndServe(":80", http.HandlerFunc(redirectTLS)); err != nil {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
+}
+
+func redirectTLS(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://www.hppgacha.art:443"+r.RequestURI, http.StatusMovedPermanently)
 }
