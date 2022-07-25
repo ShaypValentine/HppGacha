ae88c28136fe749d3b9f7695f3268f349e6c4fa2
BOCKTAELS Hugo <h-bocktaels@sfi.fr>
2022-07-25 10:37:54 +0200

    Trying to fix double roll issues with debounce

--- src/ressources/js/hpp.js
+++ src/ressources/js/hpp.js
@@ -149,3 +149,21 @@ document.addEventListener('click', function (event) {
     let flipInner = event.target.parentNode
     flipInner.style.transform = 'rotateY(180deg)'
 })
+
+
+const debouncedRoll = debounce(() => roll())
+
+function debounce(func, wait = 200, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
